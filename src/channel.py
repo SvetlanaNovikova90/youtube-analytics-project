f3d0ed90f0a42f0c048d0927570b2fecfb5cca4b
Svetlana Novikova <seaciliya2012@yandex.ru>
2024-01-26 20:12:29 +0300

    "YOU TUBE"

--- src/channel.py
+++ src/channel.py
@@ -1,16 +1,24 @@
-from helper.youtube_api_manual import youtube
+import os
+
+from googleapiclient.discovery import build
+
+from helper.youtube_api_manual import youtube, printj
 
 
 class Channel:
     """Класс для ютуб-канала"""
-    info = []
+    api_key: str = 'YT_API_KEY'
+
+    # создать специальный объект для работы с API
+    youtube = build('youtube', 'v3', developerKey=api_key)
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
     def print_info(self, ) -> None:
         """Выводит в консоль информацию о канале."""
-          # HighLoad Channel
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         return print(channel)
+
+
 
